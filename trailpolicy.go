755a9f351e9f3c0170617687f6756b975a3a543e
Ryan Caught <rcaught@gmail.com>
2016-06-13 23:22:25 +1000

    Read from stdin

--- trailpolicy.go
+++ trailpolicy.go
@@ -3,6 +3,8 @@ package main
 import (
   "encoding/json"
   "fmt"
+  "io/ioutil"
+  "os"
   "strings"
 )
 
@@ -34,53 +36,10 @@ type policyStatement struct {
 
 type action []string
 
-var data = `
-{
-    "Records": [{
-        "eventVersion": "1.0",
-        "userIdentity": {
-            "type": "IAMUser",
-            "principalId": "EX_PRINCIPAL_ID",
-            "arn": "arn:aws:iam::123456789012:user/Alice",
-            "accountId": "123456789012",
-            "accessKeyId": "EXAMPLE_KEY_ID",
-            "userName": "Alice"
-        },
-        "eventTime": "2014-03-06T21:01:59Z",
-        "eventSource": "ec2.amazonaws.com",
-        "eventName": "StopInstances",
-        "awsRegion": "us-west-2",
-        "sourceIPAddress": "205.251.233.176",
-        "userAgent": "ec2-api-tools 1.6.12.2",
-        "requestParameters": {
-            "instancesSet": {
-                "items": [{
-                    "instanceId": "i-ebeaf9e2"
-                }]
-            },
-            "force": false
-        },
-        "responseElements": {
-            "instancesSet": {
-                "items": [{
-                    "instanceId": "i-ebeaf9e2",
-                    "currentState": {
-                        "code": 64,
-                        "name": "stopping"
-                    },
-                    "previousState": {
-                        "code": 16,
-                        "name": "running"
-                    }
-                }]
-            }
-        }
-    }
-  ]
-}`
-
 func main() {
-  if cloudtrailRecords, err := parse(data); err != nil {
+  bytes, _ := ioutil.ReadAll(os.Stdin)
+
+  if cloudtrailRecords, err := parse(string(bytes)); err != nil {
     fmt.Println(fmt.Errorf("xxxxx: %s", err.Error()))
   } else {
     if val, err := createPolicy(cloudtrailRecords); err != nil {
