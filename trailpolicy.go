be67c7bbe48b4ff1f73c63d823310ff2db373594
Ryan Caught <rcaught@gmail.com>
2016-06-16 15:30:14 +1000

    go fmt

--- trailpolicy.go
+++ trailpolicy.go
@@ -1,93 +1,93 @@
 package trailpolicy
 
 import (
-  "encoding/json"
-  "fmt"
-  "strings"
+	"encoding/json"
+	"fmt"
+	"strings"
 )
 
 type cloudtrailLog struct {
-  Records []cloudtrailRecord
+	Records []cloudtrailRecord
 }
 
 type cloudtrailRecord struct {
-  EventSource string
-  EventName   string
+	EventSource string
+	EventName   string
 }
 
 type policyDocument struct {
-  Version   string
-  Statement []policyStatement
+	Version   string
+	Statement []policyStatement
 }
 
 type policyStatement struct {
-  Effect   string
-  Action   actions
-  Resource string
+	Effect   string
+	Action   actions
+	Resource string
 }
 
 type actions []string
 
 func parse(cloudtrailJSON []byte) (*[]cloudtrailRecord, error) {
-  trail := cloudtrailLog{}
+	trail := cloudtrailLog{}
 
-  if err := json.Unmarshal(cloudtrailJSON, &trail); err != nil {
-    return nil, err
-  }
+	if err := json.Unmarshal(cloudtrailJSON, &trail); err != nil {
+		return nil, err
+	}
 
-  return &trail.Records, nil
+	return &trail.Records, nil
 }
 
 func deriveAction(record cloudtrailRecord) string {
-  service := strings.Split(record.EventSource, ".")[0]
+	service := strings.Split(record.EventSource, ".")[0]
 
-  return service + ":" + record.EventName
+	return service + ":" + record.EventName
 }
 
 func createPolicy(r *[]cloudtrailRecord) (*policyDocument, error) {
-  actions := actions{}
+	actions := actions{}
 
-  for _, record := range *r {
-    action := deriveAction(record)
-    actions = append(actions, action)
-  }
+	for _, record := range *r {
+		action := deriveAction(record)
+		actions = append(actions, action)
+	}
 
-  document := policyDocument{
-    Version:   "2012-10-17",
-    Statement: []policyStatement{{Effect: "Allow", Resource: "*", Action: actions}}}
+	document := policyDocument{
+		Version:   "2012-10-17",
+		Statement: []policyStatement{{Effect: "Allow", Resource: "*", Action: actions}}}
 
-  return &document, nil
+	return &document, nil
 }
 
 func createPolicyJSON(document *policyDocument) (*[]byte, error) {
-  result, err := json.MarshalIndent(*document, "", "  ")
+	result, err := json.MarshalIndent(*document, "", "  ")
 
-  if err != nil {
-    return nil, err
-  }
+	if err != nil {
+		return nil, err
+	}
 
-  return &result, nil
+	return &result, nil
 }
 
 // Convert takes a JSON based Cloudtrail log and returns a JSON based IAM Policy Document
 func Convert(cloudtrailJSON []byte) (string, error) {
-  cloudtrailRecords, err := parse(cloudtrailJSON)
+	cloudtrailRecords, err := parse(cloudtrailJSON)
 
-  if err != nil {
-    return "", fmt.Errorf("Error parsing Cloudtrail log: %s", err.Error())
-  }
+	if err != nil {
+		return "", fmt.Errorf("Error parsing Cloudtrail log: %s", err.Error())
+	}
 
-  policy, err := createPolicy(cloudtrailRecords)
+	policy, err := createPolicy(cloudtrailRecords)
 
-  if err != nil {
-    return "", fmt.Errorf("Error creating Policy Document: %s", err.Error())
-  }
+	if err != nil {
+		return "", fmt.Errorf("Error creating Policy Document: %s", err.Error())
+	}
 
-  json, err := createPolicyJSON(policy)
+	json, err := createPolicyJSON(policy)
 
-  if err != nil {
-    return "", fmt.Errorf("Error encoding Policy Document: %s", err.Error())
-  }
+	if err != nil {
+		return "", fmt.Errorf("Error encoding Policy Document: %s", err.Error())
+	}
 
-  return string(*json), nil
+	return string(*json), nil
 }
