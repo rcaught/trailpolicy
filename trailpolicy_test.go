de277214ac08de47d925eceb6bd403a683aa74ee
Ryan Caught <rcaught@gmail.com>
2016-06-17 16:20:00 +1000

    Sort / Unique tests

--- trailpolicy_test.go
+++ trailpolicy_test.go
@@ -41,15 +41,52 @@ func TestConvertMultiple(t *testing.T) {
     {
       "Effect": "Allow",
       "Action": [
-        "ec2:StopInstances",
-        "ec2:CreateKeyPair"
+        "ec2:CreateKeyPair",
+        "ec2:StopInstances"
       ],
       "Resource": "*"
     }
   ]
 }`
 
-	assert.Equal(t, expected, result)
+  assert.Equal(t, expected, result)
+}
+
+func TestConvertDuplicates(t *testing.T) {
+  bytes, _ := ioutil.ReadFile("fixtures/convox.log")
+
+  result, _ := trailpolicy.Convert(bytes)
+
+  expected := `{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "cloudformation:ListStacks",
+        "ec2:DescribeAccountAttributes",
+        "ec2:DescribeAddresses",
+        "ec2:DescribeAvailabilityZones",
+        "ec2:DescribeHosts",
+        "ec2:DescribeInstanceStatus",
+        "ec2:DescribeInstances",
+        "ec2:DescribeKeyPairs",
+        "ec2:DescribePlacementGroups",
+        "ec2:DescribeSecurityGroups",
+        "ec2:DescribeSnapshots",
+        "ec2:DescribeStaleSecurityGroups",
+        "ec2:DescribeTags",
+        "ec2:DescribeVolumeStatus",
+        "ec2:DescribeVolumes",
+        "ec2:DescribeVpcs",
+        "elasticloadbalancing:DescribeLoadBalancers"
+      ],
+      "Resource": "*"
+    }
+  ]
+}`
+
+  assert.Equal(t, expected, result)
 }
 
 func TestConvertError(t *testing.T) {
